9dc226cc2c8a3a38263de427cea8dda169fac7ef
keremsav <krmsv19@gmail.com>
2023-05-23 21:44:29 +0300

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './landing/home/home.component';
+import { BlogListComponent } from './landing/blog-list/blog-list.component';
+import { BlogDetailComponent } from './landing/blog-detail/blog-detail.component';
+import { SignInComponent } from './landing/sign-in/sign-in.component';
+import { SignUpComponent } from './landing/sign-up/sign-up.component';
+import { WorkspaceComponent } from './landing/workspace/workspace.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogListComponent,
+    BlogDetailComponent,
+    SignInComponent,
+    SignUpComponent,
+    WorkspaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/blog.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  private apiPostUrl: string = 'http://localhost:8000/api/posts'; // Backend API'nin URL'i
+  private apiCommentUrl: string = 'http://localhost:8000/api/comments'; // Backend API'nin URL'i
+
+
+  constructor(private http: HttpClient) { }
+
+  createPost(title: string, content: string, tags: string[]): Observable<any> {
+    const body = {
+      title: title,
+      content: content,
+      tags: tags
+    };
+
+    return this.http.post<any>(`${this.apiPostUrl}`, body);
+  }
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiPostUrl}`);
+  }
+
+  getPostById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiPostUrl}/${id}`);
+  }
+
+  updatePost(id: string, title: string, content: string, tags: string[]): Observable<any> {
+    const body = {
+      title: title,
+      content: content,
+      tags: tags
+    };
+
+    return this.http.put<any>(`${this.apiPostUrl}/${id}`, body);
+  }
+
+  deletePost(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiPostUrl}/${id}`);
+  }
+
+
+  createComment(content: string, post: string): Observable<any> {
+    const body = {
+      content: content,
+      post: post
+    };
+
+    return this.http.post<any>(`${this.apiCommentUrl}`, body);
+  }
+
+  getCommentsByPostId(postId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiCommentUrl}/${postId}`);
+  }
+
+  getCommentById(commentId: string): Observable<any> {
+    return this.http.get<any>(`${this.apiCommentUrl}/comment/${commentId}`);
+  }
+
+  updateComment(commentId: string, content: string): Observable<any> {
+    const body = {
+      content: content
+    };
+
+    return this.http.put<any>(`${this.apiCommentUrl}/${commentId}`, body);
+  }
+
+  deleteComment(commentId: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiCommentUrl}/${commentId}`);
+  }
+}
+
