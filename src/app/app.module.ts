0c4c699328e1d06979256d0874b04cdd9f8e3d15
keremsav <krmsv19@gmail.com>
2023-06-05 23:14:24 +0300

    Header,footer,recent blogs done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+//import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './landing/home/home.component';
-import { BlogListComponent } from './landing/blog-list/blog-list.component';
-import { BlogDetailComponent } from './landing/blog-detail/blog-detail.component';
-import { SignInComponent } from './landing/sign-in/sign-in.component';
-import { SignUpComponent } from './landing/sign-up/sign-up.component';
-import { WorkspaceComponent } from './landing/workspace/workspace.component';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { WorkspaceComponent } from './admin/workspace/workspace.component';
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BlogItemComponent } from './blog/blog-list/blog-item/blog-item.component';
+import { CommentListComponent } from './blog/blog-detail/comment-list/comment-list.component';
+import { CommentItemComponent } from './blog/blog-detail/comment-list/comment-item/comment-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import { RecentBlogsComponent } from './blog/recent-blogs/recent-blogs.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     BlogListComponent,
     BlogDetailComponent,
     SignInComponent,
     SignUpComponent,
-    WorkspaceComponent
+    WorkspaceComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogItemComponent,
+    CommentListComponent,
+    CommentItemComponent,
+    RecentBlogsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/blog/recent-blogs/recent-blogs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-blogs',
+  templateUrl: './recent-blogs.component.html',
+  styleUrls: ['./recent-blogs.component.css']
+})
+export class RecentBlogsComponent {
+
+}
