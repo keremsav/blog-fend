2277c1ad68a9d50d57fa68109359968f69edefe0
keremsav <krmsv19@gmail.com>
2023-07-31 16:33:09 +0300

    Edit post dialog done.

--- src/app/admin-panel/post-managment/edit-post-dialog/edit-post.component.ts
+++ src/app/admin-panel/post-managment/edit-post-dialog/edit-post.component.ts
@@ -1,9 +1,6 @@
 import {Component, inject, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {BlogService} from "../../../services/blog.service";
-import {MatChip, MatChipEditedEvent, MatChipInputEvent, MatChipListbox} from "@angular/material/chips";
-import {MatChipsModule} from "@angular/material/chips";
-import {LiveAnnouncer} from "@angular/cdk/a11y";
 
 @Component({
   selector: 'app-edit-post-dialog',
@@ -21,47 +18,23 @@ export class EditPostComponent {
       console.log(error);
     })
   }
-  newTag: string = ''; // Store the new tag before adding it to the tags array
 
-  announcer = inject(LiveAnnouncer);
+  newTag: string = '';
 
-  add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    // Add our fruit
-    if (value) {
-      this.post.tags.push({name: value});
+  addTag() {
+    if (this.newTag && this.post.tags.indexOf(this.newTag) === -1) {
+      this.post.tags.push(this.newTag);
+      this.newTag = '';
     }
-
-    // Clear the input value
-    event.chipInput!.clear();
   }
 
-  remove(tag: any): void {
+  removeTag(tag: string) {
     const index = this.post.tags.indexOf(tag);
-
     if (index >= 0) {
       this.post.tags.splice(index, 1);
-
-      this.announcer.announce(`Removed ${tag}`);
     }
   }
 
-  edit(tag: any, event: MatChipEditedEvent) {
-    const value = event.value.trim();
-
-    // Remove fruit if it no longer has a name
-    if (!value) {
-      this.remove(tag);
-      return;
-    }
-
-    // Edit existing fruit
-    const index = this.post.tags.indexOf(tag);
-    if (index >= 0) {
-      this.post.tags[index].name = value;
-    }
-  }
   onSave() : void {
     this.blogService.updatePost(this.post._id, this.post.title, this.post.content , this.post.author,this.post.categoryIds,this.post.tags,this.post.image).subscribe(() => {
 
